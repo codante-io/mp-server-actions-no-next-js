048bbee91aef7d34d1a7538d2744897b34ed4c9f
Roberto Cestari <robertotcestari@gmail.com>
2024-05-03 15:17:04 -0300

    resolucao

--- components/filter-dropdown.tsx
+++ components/filter-dropdown.tsx
@@ -1,3 +1,4 @@
+'use client';
 
 import {
   DropdownMenu,
@@ -11,8 +12,29 @@ import {
 
 import { Button } from '@/components/ui/button';
 import { Filter } from 'lucide-react';
+import { useState } from 'react';
+import { usePathname, useSearchParams, useRouter } from 'next/navigation';
 
 export default function FilterDropdown() {
+  // state can be '', 'pending' or 'completed'
+  const [filterStatus, setFilterStatus] = useState('');
+  const searchParams = useSearchParams();
+  const pathname = usePathname();
+  const { replace } = useRouter();
+
+  function handleChangeFilter(value: string) {
+    const params = new URLSearchParams(searchParams);
+
+    if (value) {
+      params.set('status', value);
+    } else {
+      params.delete('status');
+    }
+
+    replace(`${pathname}?${params.toString()}`);
+    setFilterStatus(value);
+  }
+
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
@@ -29,7 +51,10 @@ export default function FilterDropdown() {
       <DropdownMenuContent className="w-16">
         <DropdownMenuLabel>Filtrar por:</DropdownMenuLabel>
         <DropdownMenuSeparator />
-        <DropdownMenuRadioGroup value="">
+        <DropdownMenuRadioGroup
+          value={filterStatus}
+          onValueChange={handleChangeFilter}
+        >
           <DropdownMenuRadioItem value="">Todos</DropdownMenuRadioItem>
           <DropdownMenuRadioItem value="pending">
             Pendente
